6d543588f19e0697eafb12e308f8bb568847b73f
o.klopskiy <abserber@gmail.com>
2022-04-14 00:46:38 +0300

    MyFirstGame add

---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,17 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int computerNum = (int) (Math.random() * (100 - 0) + 0);
+        int playerNum = 47;
+        System.out.println(computerNum);
+        while (playerNum != computerNum) {
+            if (playerNum < computerNum) {
+                System.out.println("Число " + playerNum + " меньше того, что загадал компьютер");
+                playerNum++;
+            } else if (playerNum > computerNum) {
+                System.out.println("Число " + playerNum + " больше того, что загадал компьютер");
+                playerNum--;
+            }
+        System.out.println("Вы победили");
+        }
+    }
+}
