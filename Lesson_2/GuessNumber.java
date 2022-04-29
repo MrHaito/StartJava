6a73b8bcb7d0dbc4f7f455046d7a8e4a19edb4dc
o.klopskiy <abserber@gmail.com>
2022-04-29 16:18:56 +0300

    GuessNumber fix

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -7,7 +7,6 @@ public class GuessNumber {
     private Player player1;
     private Player player2;
     private Player currentPlayer;
-    private int count = 1;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
