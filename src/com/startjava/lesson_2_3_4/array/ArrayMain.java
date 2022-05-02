333cf603c87e44846765c1a2b2fd274bbdcf3920
o.klopskiy <abserber@gmail.com>
2022-05-02 08:41:47 +0300

    ArrayMain fix

--- src/com/startjava/lesson_2_3_4/array/ArrayMain.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayMain.java
@@ -1,4 +1,26 @@
 package com.startjava.lesson_2_3_4.array;
 
 public class ArrayMain {
+    public static void main(String[] args) {
+        int[] numbers = new int[6];
+        float[] numbers1 = {5.1f, .5f, 200.5f};
+
+        System.out.println(numbers.length);
+        System.out.println(numbers1.length);
+
+        for (float v : numbers1) {
+            System.out.print(v + " ");
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i * 10;
+        }
+
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+
+        String[] text = new String[3];
+
+    }
 }
