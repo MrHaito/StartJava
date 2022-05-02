59f1c911ad6c1abb380f750f0f32511d30de801e
o.klopskiy <abserber@gmail.com>
2022-05-02 19:07:40 +0300

    GuessNumber and Player upgrade

--- src/com/startjava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -1,5 +1,6 @@
 package com.startjava.lesson_2_3_4.game;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,6 +10,13 @@ public class GuessNumber {
     private Player player1;
     private Player player2;
     private Player currentPlayer;
+    private static int globalCount = 10;
+    private int computerNum;
+    private int endOfArray;
+
+    public static int getGlobalCount() {
+        return globalCount;
+    }
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
@@ -16,21 +24,60 @@ public class GuessNumber {
     }
 
     public void start() {
-        int computerNum = random.nextInt(100) + 1;
+        resetGame();
+        computerNum = random.nextInt(100) + 1;
         System.out.println("Начинаем игру!");
+        System.out.println("У каждого игрока " + globalCount + " попыток");
+        gameProcess();
+        arraysDisplay();
+    }
+
+    private void gameProcess() {
         do {
             currentPlayer = currentPlayer == player1 ? player2 : player1;
+
             System.out.print(currentPlayer.getName() + " введите число от 0 до 100: ");
             currentPlayer.setNum(scanner.nextInt());
+            currentPlayer.addNumInArray();
+
             if (currentPlayer.getNum() < computerNum) {
                 System.out.println("Число " + currentPlayer.getNum() + " меньше того, что загадал компьютер");
             } else if (currentPlayer.getNum() > computerNum) {
                 System.out.println("Число " + currentPlayer.getNum() + " больше того, что загадал компьютер");
             } else {
-                System.out.println(currentPlayer.getName() + " победил!");
+                System.out.println("Игрок " + currentPlayer.getName() + " угадал число " + computerNum + " с " + (globalCount - currentPlayer.getCount()) + " попытки");
             }
-        } while (currentPlayer.getNum() != computerNum);
+
+            if (currentPlayer.getCount() == 0) {
+                System.out.println("У игрока " + currentPlayer.getName() + " кончились попытки");
+            }
+
+        } while (currentPlayer.getNum() != computerNum && (player1.getCount() != 0 || player2.getCount() != 0));
+    }
+
+    private void arraysDisplay() {
+        System.out.print("Числа игрока " + player1.getName() + ": ");
+        endOfArray = globalCount - player1.getCount();
+        int[] numsCopy = Arrays.copyOf(player1.getNums(), endOfArray);
+        for (int j : numsCopy) {
+            System.out.print(j + " ");
+        }
+        Arrays.fill(player1.getNums(), 0, endOfArray, 0);
+        System.out.print("\n");
+        System.out.print("Числа игрока " + player2.getName() + ": ");
+        endOfArray = globalCount - player2.getCount();
+        numsCopy = Arrays.copyOf(player2.getNums(), endOfArray);
+        for (int j : numsCopy) {
+            System.out.print(j + " ");
+        }
+        System.out.print("\n");
+        Arrays.fill(player2.getNums(), 0, endOfArray, 0);
+    }
+
+    private void resetGame() {
         player1.setNum(0);
+        player1.setCount(globalCount);
         player2.setNum(0);
+        player2.setCount(globalCount);
     }
 }
--- src/com/startjava/lesson_2_3_4/game/Player.java
+++ src/com/startjava/lesson_2_3_4/game/Player.java
@@ -3,6 +3,8 @@ package com.startjava.lesson_2_3_4.game;
 public class Player {
     private String name;
     private int num;
+    private int count = GuessNumber.getGlobalCount();
+    private int[] nums = new int[count];
 
     public Player(String name) {
         this.name = name;
@@ -12,11 +14,28 @@ public class Player {
         return name;
     }
 
+    public int getNum() {
+        return num;
+    }
+
     public void setNum(int num) {
         this.num = num;
     }
 
-    public int getNum() {
-        return num;
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int[] getNums() {
+        return nums;
+    }
+
+    void addNumInArray() {
+        nums[nums.length - count] = num;
+        count--;
     }
 }
